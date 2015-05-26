12120c28d7cd4e8a02c5fbfc657d6a390477ab80
nemtajo <nemtajo@yahoo.com>
2015-05-26 18:23:55 +0200

    removed weird annotation from file

--- Implementation/php/get_instructor_by_name.php
+++ Implementation/php/get_instructor_by_name.php
@@ -39,11 +39,7 @@ if (isset($_GET["name"])) {
 			$entry["categories"] = array();
 
 			$courses_result = mysqli_query($con,
-<<<<<<< HEAD
 				"SELECT title, course_category_id FROM course " .
-=======
-				"SELECT title FROM course " .
->>>>>>> d30c79b37de430d738a872ff25a2de4fd822dc18
 				"INNER JOIN course_instructor " .
 				"ON course_id = fk_course_id " .
 				"WHERE fk_instructor_id = " . $row['instructor_id']) or die(mysql_error());
@@ -51,7 +47,6 @@ if (isset($_GET["name"])) {
 			if (mysqli_num_rows($courses_result) > 0) {
 				$response["success"] = 1;
 				$response["message"] = "Courses and instructors found!";
-<<<<<<< HEAD
 				
 				$duplicatesContainingArray = array();
 				while ($course_row = mysqli_fetch_array($courses_result)) {
@@ -61,19 +56,6 @@ if (isset($_GET["name"])) {
 					//join $course_row["course_category_id"] with category.category_id and take category.name
 					$categories_result = mysqli_query($con,
 						"SELECT name FROM category " .
-=======
-
-				while ($course_row = mysqli_fetch_array($courses_result)) {
-					//get title of the course
-					array_push($entry["courses"], $course_row["title"]);
-					echo "passed";
-					echo $course_row["course_category_id"];
-					//join $course_row["course_category_id"] with category.category_id and take category.name
-					$categories_result = mysqli_query($con,
-						"SELECT name FROM category " .
-						//"INNER JOIN course " .
-						//"ON course_category_id = category_id " .
->>>>>>> d30c79b37de430d738a872ff25a2de4fd822dc18
 						"WHERE category_id = " . $course_row["course_category_id"]) or die(mysql_error());
 						
 					if (mysqli_num_rows($categories_result) > 0) {
@@ -82,25 +64,18 @@ if (isset($_GET["name"])) {
 						
 						while ($category_row = mysqli_fetch_array($categories_result)) {
 							//get name of the category
-<<<<<<< HEAD
 							array_push($duplicatesContainingArray, $category_row["name"]);
-=======
-							array_push($entry["categories"], $category_row["name"]);
->>>>>>> d30c79b37de430d738a872ff25a2de4fd822dc18
 						}
 					} else {
 							$response["success"] = 0;
 							$response["message"] = "Categories not found!";
 					}
 				}
-<<<<<<< HEAD
 				$duplicatesRemovedArray = array_unique($duplicatesContainingArray);
 				foreach ($duplicatesRemovedArray as &$categoryName) {
 					array_push($entry["categories"], $categoryName);
 				}
 				
-=======
->>>>>>> d30c79b37de430d738a872ff25a2de4fd822dc18
 			} else {
 				$response["success"] = 0;
 				$response["message"] = "Courses not found!";
