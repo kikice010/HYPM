205b635f040500207fa3ca86e0207e4e8ca65689
mirjamsk <mirjamskarica@gmail.com>
2015-05-26 03:04:02 +0200

    Add breadcrumb type of thing for single course

--- Implementation/resources/js/singleCourse.js
+++ Implementation/resources/js/singleCourse.js
@@ -4,11 +4,12 @@ $(document).ready(function() {
 
     function createCourseContainer(course) { // clone the empty boilerplate node and fill it with content
         var rootElement = $('<div>');
-        rootElement.append($('<h1>').html(course.title));
+        $('.page-header h1').html(course.title);
+        $('.breadcrumb li:last').html(course.title);
 
-        rootElement.append($('<div>').text(course.level));
-        rootElement.append($('<div>').text(course.room));
-        rootElement.append($('<div>').text(course.category));
+        rootElement.append($('<div>').html(course.level));
+        rootElement.append($('<div>').html(course.room));
+        rootElement.append($('<div>').html(course.category));
         rootElement.append($('<div>').html(course.description));
         rootElement.append($('<div>').html(course.target));
 
