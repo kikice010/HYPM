fef28a5da80b3b88e5e82854391568848c82b356
mirjamsk <mirjamskarica@gmail.com>
2015-06-14 19:09:47 +0200

    Add error messages when wrong params passed to php functions or w/o internet connection#

--- WEBSITE/resources/js/loadContentFunction.js
+++ WEBSITE/resources/js/loadContentFunction.js
@@ -5,12 +5,21 @@ function loadContent(phpUrl, rootJSON, $container, function_createContentElement
         data:  getParams,
         dataType: 'json',
         success: function(data) {
+            console.log(data);
             if (data.hasOwnProperty(rootJSON)) {
                 for (var i in data[rootJSON]) {
                     $container.append(
                         function_createContentElement(data[rootJSON][i]));
                 }
             }
+            if (data.hasOwnProperty('success') && data.success === 0){
+                $('.page-wrap .container').append(
+                    $('<p>Invalid '+rootJSON+' request</p>').addClass('error-msg'));
+            }
+        },
+        error: function(requestObject, error, errorThrown) {
+            $('.page-wrap .container').append(
+                $('<p>Please check your internet connection</p>').addClass('error-msg'));
         }
     });
 }
